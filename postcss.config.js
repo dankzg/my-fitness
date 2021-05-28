4972b390592fea89e024a5e8b0a4c76e6abf6ed7
dank.zheng <dank.zheng@mplus.tech>
2021-05-28 15:02:15 +0800

    new file

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Company: Dank
+ * @Author: dank.zheng
+ * @Date: 2021-05-21 10:44:34
+ * @LastEditors: dank.zheng
+ * @LastEditTime: 2021-05-21 10:44:45
+ */
+module.exports = {
+  plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+  }
+}
